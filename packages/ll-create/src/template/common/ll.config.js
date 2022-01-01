05aafc7804f94dca600cbf2cbc7fd7d4dd19556b
TrickyPi <530257315@qq.com>
2022-01-01 16:29:13 +0800

    [ll-create]<feat>:complete template

--- packages/ll-create/src/template/common/ll.config.js
+++ packages/ll-create/src/template/common/ll.config.js
@@ -1,5 +1,8 @@
-const httpProxy = require("http-proxy-middleware");
-
 module.exports = {
+  webpackConfig: (prevConfig, { isDev, isBuild }) => {
+    //the best way is use webpack-merge to merge webpack config
+    return prevConfig;
+  },
+  //webpack-dev-server config
   devServer: {}
 };
