08aac82c2115ecde1323b00a2f1b4f861a5a162b
TrickyPi <530257315@qq.com>
2022-01-09 14:25:04 +0800

    update template

--- packages/ll-create/src/template/common/ll.config.js
+++ packages/ll-create/src/template/common/ll.config.js
@@ -1,8 +1,11 @@
+/** @typedef {import("ll-script/lib/config/webpack.config").WebpackConfig} WebpackConfig */
+/** @typedef {import("ll-script/lib/scripts/start").Configuration} DevServerConfig */
 module.exports = {
+  /** @type {WebpackConfig} */
   webpackConfig: (prevConfig, { isDev, isBuild }) => {
     //the best way is use webpack-merge to merge webpack config
     return prevConfig;
   },
-  //webpack-dev-server config
+  /** @type {DevServerConfig} */
   devServer: {}
 };
